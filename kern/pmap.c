13d6301371bc57f5cf29a8ac9e928e78410d7de9
KeKeR <macworlex@gmail.com>
2017-10-24 18:18:40 +0300

    Lab 6 Done + small change in page_free

--- kern/pmap.c
+++ kern/pmap.c
@@ -290,7 +290,7 @@ page_free(struct PageInfo *pp)
 	// Fill this function in
 	// Hint: You may want to panic if pp->pp_ref is nonzero or
 	// pp->pp_link is not NULL.
-	if ((pp->pp_ref != 0) && (pp->pp_link != NULL)) {
+	if ((pp->pp_ref != 0) || (pp->pp_link != NULL)) {
 		panic("page_free");
 	}
 	pp->pp_link = page_free_list;
