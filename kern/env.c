1de35bba2e67d17792a4d4923bb07af9b76ebd88
KeKeR <macworlex@gmail.com>
2017-12-05 15:42:08 +0300

    Lab 10 Done

--- kern/env.c
+++ kern/env.c
@@ -463,6 +463,7 @@ load_icode(struct Env *e, uint8_t *binary, size_t size)
 	// at virtual address USTACKTOP - PGSIZE.
 	// LAB 8: Your code here.
 	region_alloc(e, (void *)(USTACKTOP - PGSIZE), PGSIZE);
+	lcr3(PADDR(kern_pgdir));
 }
 
 //
